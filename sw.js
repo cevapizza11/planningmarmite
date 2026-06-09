45f49991839d00fc4925903d7f6a6230d558591b
cevapizza11 <cevapizza11@gmail.com>
2026-06-09 08:21:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,5 @@
-const CACHE = 'marmite-planning-v18';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('jsonbin.io')) return;
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+const V = 'marmite-v19';
+const CACHE = ['./','./index.html','./manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(CACHE))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { if(e.request.url.includes('jsonbin.io'))return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });

fae9c0f2d403cb977e8b17f9a05b3638fcb6ce25
cevapizza11 <cevapizza11@gmail.com>
2026-06-09 09:23:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'marmite-v20';
+const V = 'marmite-v21';
 const CACHE = ['./','./index.html','./manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(CACHE))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k))))); self.clients.claim(); });
